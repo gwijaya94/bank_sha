198707dfbf1c071228a4ca67b8ffc6ceb300f008
Gilbert <gilbertwijaya94@gmail.com>
2023-06-23 01:34:52 +0700

    config theme for colors

---
+++ lib/shared/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color whiteColor = const Color(0xffFFFFFF);
+Color blackColor = const Color(0xff14193F);
+Color greyColor = const Color(0xffA4A8AE);
+Color blueColor = const Color(0xff53C1F9);
+Color purpleColor = const Color(0xff5142E6);
+Color greenColor = const Color(0xff22B07D);
+Color redColor = const Color(0xffFF2566);
+
+Color lightBgColor = const Color(0xffF6F8FB);
+Color darkBgColor = const Color(0xff020518);
+Color numberBgColor = const Color(0xff1A1D2E);
