b29ed04ed8d1d04c7088fcff030b48f4f9122030
Gilbert <gilbertwijaya94@gmail.com>
2023-06-23 23:45:17 +0700

    config theme for text color, font weight

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color whiteColor = const Color(0xffFFFFFF);
 Color blackColor = const Color(0xff14193F);
@@ -11,3 +12,19 @@ Color redColor = const Color(0xffFF2566);
 Color lightBgColor = const Color(0xffF6F8FB);
 Color darkBgColor = const Color(0xff020518);
 Color numberBgColor = const Color(0xff1A1D2E);
+
+// TEXT COLOR
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
+TextStyle blueTextStyle = GoogleFonts.poppins(color: blueColor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: greenColor);
+
+// FONT WEIGHT
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
