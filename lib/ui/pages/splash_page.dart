5587d012e7ebf35f6c7bffb5e9cbbfe77680b22d
Gilbert <gilbertwijaya94@gmail.com>
2023-06-24 00:37:38 +0700

    add Splash Page

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,11 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
