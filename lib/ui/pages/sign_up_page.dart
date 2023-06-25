690417605d667a223e9c61afda3df8dc4e62646e
Gilbert <gilbertwijaya94@gmail.com>
2023-06-26 00:56:59 +0700

    - UI Sign Up Page

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatelessWidget {
@@ -8,6 +9,80 @@ class SignUpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBgColor,
+      body: ListView(
+        padding: const EdgeInsets.all(24),
+        children: [
+          Container(
+            width: 155,
+            height: 50,
+            margin: const EdgeInsets.only(
+              top: 100,
+              bottom: 100,
+            ),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "assets/img_logo_light.png",
+                ),
+              ),
+            ),
+          ),
+          Text(
+            "Join Us to Unlock\nYour Growth",
+            style: blackTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const InputForm(
+                  label: "Full Name",
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const InputForm(
+                  label: "Email Address",
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const InputForm(
+                  label: "Password",
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  onPressed: () {},
+                  title: 'Continue',
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomTextButton(
+            title: "Sign In",
+            onPressed: () {
+              Navigator.pushNamed(context, "/sign-in");
+            },
+          ),
+        ],
+      ),
     );
   }
 }
