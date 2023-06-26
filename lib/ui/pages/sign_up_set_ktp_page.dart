0a8b90aa953e4861b8c72a0a8b82519deff1205c
Gilbert <gilbertwijaya94@gmail.com>
2023-06-26 21:45:50 +0700

    - UI Sign Up KTP Profile Page

--- lib/ui/pages/sign_up_set_ktp_page.dart
+++ lib/ui/pages/sign_up_set_ktp_page.dart
@@ -1,4 +1,5 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 
 class SignUpProfileKTPPage extends StatelessWidget {
@@ -8,6 +9,102 @@ class SignUpProfileKTPPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBgColor,
+      body: ListView(
+        padding: const EdgeInsets.all(24),
+        children: [
+          Container(
+            width: 155,
+            height: 50,
+            margin: const EdgeInsets.only(
+              top: 100,
+              bottom: 100,
+            ),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "assets/img_logo_light.png",
+                ),
+              ),
+            ),
+          ),
+          Text(
+            "Join Us to Unlock\nYour Growth",
+            style: blackTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    color: lightBgColor,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    child: Image.asset(
+                      "assets/ic_upload.png",
+                      width: 32,
+                      height: 32,
+                    ),
+                  ),
+                ),
+                // ? Uploaded Profile
+                // Container(
+                //   width: 120,
+                //   height: 120,
+                //   decoration: BoxDecoration(
+                //     color: lightBgColor,
+                //     shape: BoxShape.circle,
+                //     image: const DecorationImage(
+                //       fit: BoxFit.cover,
+                //       image: AssetImage("assets/img_profile.png"),
+                //     ),
+                //   ),
+                // ),
+
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Passport/ID Card",
+                  style: blackTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: medium,
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 50,
+                ),
+                CustomFilledButton(
+                  onPressed: () {},
+                  title: 'Continue',
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          CustomTextButton(
+            title: "Skip for Now",
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
