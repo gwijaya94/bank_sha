c91f0aaf93f1eda0f26ca3b60a90239c94d0ec6c
Gilbert <gilbertwijaya94@gmail.com>
2023-06-26 22:08:32 +0700

    - UI Sign Up Status Page

--- lib/ui/pages/sign_up_status_page.dart
+++ lib/ui/pages/sign_up_status_page.dart
@@ -1,4 +1,5 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 
 class SignUpStatusPage extends StatelessWidget {
@@ -8,6 +9,39 @@ class SignUpStatusPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBgColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Akun Berhasil\nTerdaftar",
+              textAlign: TextAlign.center,
+              style: blackTextStyle.copyWith(
+                fontWeight: semiBold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            Text(
+              "Grow your finance start\ntogether with us",
+              textAlign: TextAlign.center,
+              style: greyTextStyle.copyWith(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            CustomFilledButton(
+              title: "Get Started",
+              width: 185,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
