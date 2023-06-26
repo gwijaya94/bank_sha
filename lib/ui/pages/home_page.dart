e1a855496ccf5efe9dddadac0bbcd1b2fc484b08
Gilbert <gilbertwijaya94@gmail.com>
2023-06-26 22:51:50 +0700

    add BottomNavigationBar on Home Page

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,6 +8,51 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBgColor,
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: blueColor,
+        unselectedItemColor: blackColor,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedLabelStyle: blueTextStyle.copyWith(
+          fontSize: 10,
+          fontWeight: medium,
+        ),
+        unselectedLabelStyle: blackTextStyle.copyWith(
+          fontSize: 10,
+          fontWeight: medium,
+        ),
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/ic_overview.png",
+              width: 20,
+              color: blueColor,
+            ),
+            label: "Overview",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/ic_history.png",
+              width: 20,
+            ),
+            label: "History",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/ic_statistic.png",
+              width: 20,
+            ),
+            label: "Statistic",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/ic_reward.png",
+              width: 20,
+            ),
+            label: "Reward",
+          ),
+        ],
+      ),
     );
   }
 }
