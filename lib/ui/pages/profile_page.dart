a6b8e4df77d605fd5a36fc1694c0160ea2feb202
Gilbert <gilbertwijaya94@gmail.com>
2023-06-28 00:19:02 +0700

    UI Profile Page

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -6,13 +7,67 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: lightBgColor,
-      appBar: AppBar(
-        title: Text("My Profile"),
+    return Screen(
+      appBar: const AppBarHeader(
+        title: "My Profile",
       ),
-      body: SafeArea(
-        child: Container(),
+      child: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 30,
+        ),
+        children: [
+          Container(
+            padding: const EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor,
+            ),
+            child: Column(
+              children: [
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(120),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/img_profile.png"),
+                    ),
+                  ),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 28,
+                      height: 28,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: whiteColor,
+                      ),
+                      child: Icon(
+                        Icons.check_circle,
+                        color: greenColor,
+                        size: 24,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Shayna Hanna",
+                  style: blackTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: medium,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
