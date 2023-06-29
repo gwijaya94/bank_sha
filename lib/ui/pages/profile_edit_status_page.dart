ce17fd1fb40c0fb80e4df2cbb0c60831e4d2a30a
Gilbert <gilbertwijaya94@gmail.com>
2023-06-29 23:13:20 +0700

    UI ProfileEditStatusPage

--- lib/ui/pages/profile_edit_status_page.dart
+++ lib/ui/pages/profile_edit_status_page.dart
@@ -1,3 +1,5 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 
 class ProfileEditStatusPage extends StatelessWidget {
@@ -5,6 +7,46 @@ class ProfileEditStatusPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Screen(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Nice Update!",
+              textAlign: TextAlign.center,
+              style: blackTextStyle.copyWith(
+                fontWeight: semiBold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            Text(
+              "Your data has been updated.",
+              textAlign: TextAlign.center,
+              style: greyTextStyle.copyWith(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            CustomFilledButton(
+              title: "My Profile",
+              width: 185,
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  "/home",
+                  (route) => false,
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
