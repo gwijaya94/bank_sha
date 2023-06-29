db857778a7541a663872b2718d6144936d97ab60
Gilbert <gilbertwijaya94@gmail.com>
2023-06-30 00:55:00 +0700

    UI TopupAmountPage

--- lib/ui/pages/topup_amount_page.dart
+++ lib/ui/pages/topup_amount_page.dart
@@ -1,13 +1,167 @@
+import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/index.dart';
+import 'package:bank_sha/utils/index.dart';
 import 'package:flutter/material.dart';
 
-class TopupAmoutPage extends StatelessWidget {
+class TopupAmoutPage extends StatefulWidget {
   const TopupAmoutPage({Key? key}) : super(key: key);
 
   @override
+  State<TopupAmoutPage> createState() => _TopupAmoutPageState();
+}
+
+class _TopupAmoutPageState extends State<TopupAmoutPage> {
+  final TextEditingController inputController =
+      TextEditingController(text: "0");
+
+  addAmount(String number) {
+    setState(() {
+      if (inputController.text == "0") {
+        inputController.text = "";
+      }
+      inputController.text = inputController.text + number.toString();
+    });
+  }
+
+  deleteAmount() {
+    if (inputController.text.isNotEmpty) {
+      setState(() {
+        String textValue = inputController.text;
+        inputController.text = textValue.substring(0, textValue.length - 1);
+
+        if (inputController.text.isEmpty) {
+          inputController.text = "0";
+        }
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Size screenSize = getScreenSize(context);
+    double padHorizontal = 58;
+    double gap = 40;
+
     return Screen(
-      child: Container(),
+      backgroundColor: darkBgColor,
+      appBar: AppBarHeader(
+        title: "Total Amount",
+        titleStyle: whiteTextStyle.copyWith(
+          fontSize: 20,
+          fontWeight: semiBold,
+        ),
+        iconTheme: IconThemeData(
+          color: whiteColor,
+        ),
+        backgroundColor: darkBgColor,
+      ),
+      child: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: padHorizontal,
+          vertical: 30,
+        ),
+        children: [
+          const SizedBox(
+            height: 37,
+          ),
+          SizedBox(
+            width: screenSize.width - padHorizontal * 2,
+            child: TextFormField(
+              enabled: false,
+              controller: inputController,
+              decoration: InputDecoration(
+                suffixIconColor: greenColor,
+                contentPadding: const EdgeInsets.only(right: 0),
+                disabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: greyColor,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: greyColor,
+                  ),
+                ),
+                prefixText: "Rp.",
+                prefixStyle: whiteTextStyle.copyWith(
+                  fontSize: 36,
+                  fontWeight: medium,
+                ),
+              ),
+              style: whiteTextStyle.copyWith(
+                fontSize: 36,
+                fontWeight: medium,
+              ),
+              textAlign: TextAlign.end,
+            ),
+          ),
+          const SizedBox(
+            height: 66,
+          ),
+          Center(
+            child: Wrap(
+              spacing: gap,
+              runSpacing: gap,
+              children: [
+                ..._buildButton1to9(addAmount),
+                const SizedBox(
+                  width: 60,
+                  height: 60,
+                ),
+                CustomInputButton(
+                  title: "0",
+                  onTap: () {
+                    addAmount("0");
+                  },
+                ),
+                CustomInputButton(
+                  onTap: () {
+                    deleteAmount();
+                  },
+                  contentWidget: Icon(
+                    Icons.arrow_back,
+                    color: whiteColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          CustomFilledButton(
+            title: "Checkout Now",
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          CustomTextButton(
+            title: "Terms & Condition",
+            onPressed: () {},
+          )
+        ],
+      ),
     );
   }
+}
+
+List<Widget> _buildButton1to9(void Function(String)? addAmount) {
+  int index = 0;
+  List<Widget> listButton = [];
+
+  while (index < 9) {
+    String numberString = (index + 1).toString();
+    listButton.add(
+      CustomInputButton(
+        title: numberString,
+        onTap: () {
+          addAmount!(numberString);
+        },
+      ),
+    );
+    index++;
+  }
+
+  return listButton;
 }
