13087cd46aa63eea5a5310b53179fed04b22266e
Gilbert <gilbertwijaya94@gmail.com>
2023-06-30 01:28:23 +0700

    UI TopupStatusPage

--- lib/ui/pages/topup_status_page.dart
+++ lib/ui/pages/topup_status_page.dart
@@ -1,3 +1,4 @@
+import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +8,45 @@ class TopupStatusPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Screen(
-      child: Container(),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Top Up\nWallet Success",
+              textAlign: TextAlign.center,
+              style: blackTextStyle.copyWith(
+                fontWeight: semiBold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            Text(
+              "Use the money wisely and\ngrow your finance",
+              textAlign: TextAlign.center,
+              style: greyTextStyle.copyWith(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            CustomFilledButton(
+              title: "Back To Home",
+              width: 185,
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  "/home",
+                  (route) => false,
+                );
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
